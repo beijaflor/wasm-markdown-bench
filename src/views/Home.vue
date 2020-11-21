a5f4f928b297b0cfeac89b769a02baf664d65ab8
beijaflor <sho.otani@gmail.com>
2020-11-21 16:33:01 +0900

    measure and show convert performance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
   <section class="main-section">
     <div class="columnLeft">
-      <textarea class="source-textarea" v-model="source.content" @blur="updateHandler" />
+      <textarea class="source-textarea" v-model="source.content" @keyup="updateHandler" />
     </div>
     <div class="columnRight">
-      <textarea class="source-textarea" v-model="source.html" disabled />
+      <section class="output-htmlarea" v-html="source.html" />
     </div>
+  </section>
+  <section class="status-container">
+    <dl class="performance-list">
+      <dt class="listItem">lategst:&nbsp;</dt>
+      <dd class="listItem">{{ status.latest }}</dd>
+      <dt class="listItem" style="margin-left: 20px;">average:&nbsp;</dt>
+      <dd class="listItem">{{ status.average }}</dd>
+    </dl>
   </section>
 </template>
 
@@ -19,22 +27,37 @@ export default defineComponent({
   components: {},
   setup() {
     const source = reactive<{ content: string; html: string }>({ content: sampleMd, html: '' });
+    const status = reactive<{ latest: number; average: number }>({ latest: 0, average: 0 });
+
     const updateHtml = () => {
       const markedHtml = marked(source.content);
       source.html = markedHtml;
     };
 
-    const updateHandler = (e: FocusEvent) => {
-      const target = e.target as HTMLTextAreaElement;
-      if (target) target.disabled = true;
+    const updateSpeed = (start: number, end: number) => {
+      const duration = end - start;
+      status.latest = Math.floor(duration * 1000) / 1000;
+      status.average = Math.floor(((status.average + duration) / 2) * 1000) / 1000;
+    };
+
+    const updateHandler = () => {
+      // const updateHandler = (e: KeyboardEvent) => {
+      // const target = e.target as HTMLTextAreaElement;
+      // if (target) target.disabled = true;
+      const start = performance.now();
 
       updateHtml();
 
-      setTimeout(() => {
-        target.disabled = false;
-      }, 1000);
+      const end = performance.now();
+
+      updateSpeed(start, end);
+      // setTimeout(() => {
+      //   target.disabled = false;
+      // }, 1000);
     };
-    return { source, updateHandler };
+
+    updateHtml();
+    return { source, status, updateHandler };
   },
 });
 </script>
@@ -49,6 +72,7 @@ export default defineComponent({
     width: 45vw;
     height: 80vh;
     border: 1px solid #ddd;
+    overflow-y: scroll;
   }
 }
 
@@ -57,5 +81,28 @@ export default defineComponent({
   height: 100%;
   padding: 20px;
   border: none;
+}
+
+.output-htmlarea {
+  padding: 20px;
+  text-align: left;
+}
+
+.status-container {
+  height: 5vh;
+  background: #f0f0f0;
+}
+
+.performance-list {
+  text-align: right;
+  padding: 0 10px;
+
+  > .listItem {
+    line-height: 5vh;
+    list-style: none;
+    font-size: 11px;
+    display: inline;
+    color: #555;
+  }
 }
 </style>
