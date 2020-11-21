59e9ea2f63b41bdc2354ccc262e2b4d7fcf93ffd
beijaflor <sho.otani@gmail.com>
2020-11-21 14:31:58 +0900

    move installed infomation to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'About',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
