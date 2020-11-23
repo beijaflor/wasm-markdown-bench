b182f43860a436a4fb4b27bea9cc831fcc34fe1b
beijaflor <sho.otani@gmail.com>
2020-11-24 08:35:58 +0900

    add pulldowm-cmark and show on alert

--- wasm-markdown/src/lib.rs
+++ wasm-markdown/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use pulldown_cmark::{html, Options, Parser};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +10,22 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm-markdown!");
+    let markdown_input = "Hello world, this is a ~~complicated~~ *very simple* example.";
+
+    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+    // and we therefore must enable it explicitly.
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(markdown_input, options);
+    // Write to String buffer.
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    alert(&html_output);
 }
